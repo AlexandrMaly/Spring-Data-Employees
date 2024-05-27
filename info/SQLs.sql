338ff839dd1b3d8f4e7cc820e4b5c0bc209aa67a
Aleks <feniksizvdv@ukr.net>
2024-05-27 21:40:28 +0300

    Initial commit

---
+++ info/SQLs.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE db_employees;
+
+CREATE TABLE IF NOT EXISTS employees
+( id INTEGER NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  position VARCHAR(255) NOT NULL,
+  phone VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
